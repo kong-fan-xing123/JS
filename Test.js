73385552ab41eb50c035fa0f53d19073e0c11fc9
kong-fan-xing123 <kfstary@outlook.com>
2020-05-07 08:47:03 +0800

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,14 @@
+/*
+Test.js
+Debug the script
+*/
+function Debug(msg,win,type,DebugNumber){
+if(!DebugNumber || DebugNumber === 0 )DebugNumber=1
+if(!win)win = window;
+for ( i=0;i<DebugNumber;i++){
+if(!win.console)return null;
+win.console[type](msg
+)}
+return {DebugTypeAll:win.console,YourSelType:win.console[type]}
+}
+
